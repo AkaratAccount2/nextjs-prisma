0730df1102771b7a0cb21aac98cd664e9ae163a3
akarat <akrs33@gmail.com>
2023-11-10 13:41:51 +0700

    add code from prima

---
+++ pages/api/keep-alive.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handle(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+      const aliveCount = await prisma.keep_alive.count();
+      
+      res.status(200).json({
+        keep_alive: aliveCount
+      });
+  };
+
+
