dc727a319405cbd8a58e96593412bbe62e2a6088
akarat <akrs33@gmail.com>
2023-11-10 14:13:04 +0700

    Change path of import

--- pages/api/keep-alive.ts
+++ pages/api/keep-alive.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./../../prisma/generated/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
